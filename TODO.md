39006b033b27d83a481e2bb94d4b4d3e879a856d
Ryan Nash <ryan.nash.personal@gmail.com>
2025-07-10 01:00:43 +0100

    Nvim config updates

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# Todo 
+
+- [] All popup borders rounded, e.g. documentation hover, lazyvim, noice, mason
+- [] Snacks.nvim terminal does not position for typing correctly if reopened, only if opening a new terminal
+  - Fixed by pressing enter, can we do an autocmd?
+- [] Set up markdown, js/ts/json files, and vimtex
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  opts = {
+    -- The default style. You can change it to "storm", "night", "moon", or "day".
+    style = "storm",
+
+    -- Enable this to disable setting the background color
+    transparent = true,
+
+    -- You can configure other settings here, but the above are the most important
+    -- for your request. For example, to customize highlights:
+    styles = {
+      comments = { italic = true },
+      keywords = { italic = true },
+      -- Background styles. By default, these are unused when `transparent` is true.
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
